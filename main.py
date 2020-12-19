6cfaa7f03789105504d896a80d16b45e1b04d07b
SeanBlend <awesome.sean.haha@gmail.com>
2020-12-19 15:57:39 -0800

    added coin and stuff

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ DISPLAY = pygame.display.set_mode(SCREEN, 0, 32)
 pygame.display.set_caption("Platformer Game - By Tools With Code")
 
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+STDFONT = pygame.font.SysFont("comicsans", 60)
 
 def Main():
     FPS = 60
     clock = pygame.time.Clock()
     player = Player(WIDTH // 2, HEIGHT // 2)
-    coin = Coin(WIDTH // 2, HEIGHT // 2)
+    coin = Coin(300, HEIGHT // 2)
     score = 0
     while True:
         clock.tick(FPS)
@@ -26,7 +29,8 @@ def Main():
         coin.Draw(DISPLAY)
         if coin.CollideWithPlayer(player):
             score += 1
-            print(score)
+        scoreText = STDFONT.render(str(score), 1, WHITE)
+        DISPLAY.blit(scoreText, (20, 20))
 
         for event in events:
             if event.type == pygame.QUIT:
