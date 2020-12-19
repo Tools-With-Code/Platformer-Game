8ffebd92890cbace92a6b5d41b0c8c90720ab806
SeanBlend <awesome.sean.haha@gmail.com>
2020-12-18 21:20:14 -0800

    trying to add jump; not working

--- classes.py
+++ classes.py
@@ -7,9 +7,10 @@ class Player:
     WIDTH = 40
     HEIGHT = 80
     VEL = 5
-    JUMP_HEIGHT = math.sqrt(49)
+    JUMP_HEIGHT = 7
     NGTV_JUMP_HEIGHT = 0 - JUMP_HEIGHT
     def __init__(self, x, y):
+        self.jump = False
         self.x = x
         self.y = y
         self.color = (255, 255, 255)
@@ -18,26 +19,24 @@ class Player:
         pygame.draw.rect(display, self.color, (self.x, self.y, self.WIDTH, self.HEIGHT))
 
     def Move(self, keys):
-        jump = False
-        if not jump:
+        if not self.jump:
             if keys[pygame.K_LEFT] or keys[pygame.K_a]:
                 self.x -= self.VEL
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                 self.x += self.VEL
-            if keys[pygame.K_SPACE] or keys[pygame.K_UP] or keys[pygame.K_w]:
-                jump = True
+            if keys[pygame.K_SPACE] or keys[pygame.K_UP]:
+                self.jump = True
         else:
             if keys[pygame.K_LEFT] or keys[pygame.K_a]:
                 self.x -= self.VEL
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                 self.x += self.VEL
             if self.JUMP_HEIGHT >= self.NGTV_JUMP_HEIGHT:
-                
                 neg = 1
                 if self.JUMP_HEIGHT < 0:
                     neg = -1
-                self.y -= (self.JUMP_HEIGHT ** 2) * neg
+                self.y -= self.JUMP_HEIGHT * neg
                 self.JUMP_HEIGHT -= 1
             else:
                 self.jump = False
-                self.JUMP_HEIGHT = math.sqrt(49)
+                self.JUMP_HEIGHT = 7
